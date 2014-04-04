0a763bee2c9b1574e247e42ad4062dbbc802fe23
Wender <wender.lima@gmail.com>
2014-04-04 12:29:08 -0300

    Applying correct syntax

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ angular-simple-input-mask
 =========================
 
 Way simple Angular directive to apply mask to input fields
+
+Mask format uses 0 
+Example: 0000-0000-0000-0000
+
+Directive will look for attribute mask on input
+Example: <input name="test" maxlengh="19" mask="0000-0000-0000-0000" />
---
+++ src/angular-simple-mask.js
@@ -0,0 +1,38 @@
+angular.module('angularMask', [])
+  .directive('angularMask', function() {
+  return {
+    restrict : 'A',
+    link: function($scope, el, attrs) {
+      var format = attrs.angularMask;
+
+      function mask(el, format) {
+        var text = el;
+        var value = text.value;
+        var newValue = "";
+        for (var vId = 0, mId = 0 ; mId < format.length ; ) {
+          if (mId >= value.length){
+            break;
+          }
+          if (format[mId] == '0' && value[vId].match(/[0-9]/) === null) {
+            break;
+          }
+          while (format[mId].match(/[0\*]/) === null) {
+            if (value[vId] == format[mId]){
+              break;
+            }
+            newValue += format[mId++];
+          }
+          newValue += value[vId++];
+          mId++;
+        }
+        text.value = newValue;
+      }
+
+      el.bind('keyup keydown', function(e) {
+        var _format = format;
+        var _el = el[0];
+        mask(_el,_format);
+      });
+    }
+  };
+});
